5c96ad924ebad58f71b993a38c8f1a4798469dd9
Felix Ayoola <flxayoola@gmail.com>
2025-11-21 10:59:27 +0100

    code finaly satisfies requirement

--- wc.go
+++ wc.go
@@ -1,73 +1,99 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func wc() (string, string) {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	input := strings.Fields(scanner.Text())
+	args := os.Args[1:]
+	wantM, wantL, wantC, wantW := false, false, false, false
+	var filename string
 
-	if len(input) == 1 {
-		filename := input[0]
-		fileInfo, statErr := os.Stat(filename)
-		f, err := os.ReadFile(filename)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error:", err)
-			fmt.Fprintln(os.Stderr, "Error:", statErr)
-		}
-		if statErr != nil {
-			fmt.Fprintln(os.Stderr, "Error:", statErr)
-			return "Error!", filename
-		}
-		size := fileInfo.Size()
-		lines := strings.Count(string(f), "\n")
-		words := strings.Fields(string(f))
-		return fmt.Sprintf("%d %d %d", lines, len(words), size), filename
-	}
-
-	if len(input) == 2 {
-		toDo := input[0]
-		filename := input[1]
-
-		if toDo == "-c" {
-			fileInfo, statErr := os.Stat(filename)
-			if statErr != nil {
-				fmt.Fprintln(os.Stderr, "Error:", statErr)
-				return "Error!", filename
+	for _, a := range args {
+		switch a {
+		case "-m":
+			wantM = true
+		case "-l":
+			wantL = true
+		case "-c":
+			wantC = true
+		case "-w":
+			wantW = true
+		default:
+			if filename == "" {
+				filename = a
 			}
-			return fmt.Sprintf("%d", fileInfo.Size()), filename
-		}
 
-		f, err := os.ReadFile(filename)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error:", err)
-			return "Error!", filename
 		}
-
-		if toDo == "-l" {
-			lines := strings.Count(string(f), "\n")
-			return fmt.Sprintf("%d", lines), filename
-		}
-		if toDo == "-w" {
-			words := strings.Fields(string(f))
-			return fmt.Sprintf("%d", len(words)), filename
-		}
-		if toDo == "-m" {
-			chars := len([]rune(string(f)))
-			return fmt.Sprintf("%d", chars), filename
-		}
-		return "Error!", filename
 	}
 
-	return "Error!", ""
+	if !wantL && !wantC && !wantW && !wantM {
+		wantL = true
+		wantC = true
+		wantW = true
+	}
+
+	var data []byte
+	var err error
+	sourceIsFile := false
+
+	//if there is file name
+	if filename != "" {
+		data, err = os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading file ", err)
+			return "Error", filename
+		}
+		sourceIsFile = true
+	} else { //no file name, read from stdin
+		data, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading stdin: ", err)
+			return "Error", ""
+		}
+	}
+
+	bytesCount := len(data)
+	linesCount := strings.Count(string(data), "\n")
+	if len(data) > 0 && data[len(data)-1] != '\n' {
+		linesCount++
+	}
+	wordsCount := len(strings.Fields(string(data)))
+	charsCount := len([]rune(string(data)))
+
+	var output []string
+	if wantL {
+		output = append(output, fmt.Sprintf("%d", linesCount))
+	}
+	if wantW {
+		output = append(output, fmt.Sprintf("%d", wordsCount))
+	}
+	if wantC {
+		output = append(output, fmt.Sprintf("%d", bytesCount))
+	}
+	if wantM {
+		output = append(output, fmt.Sprintf("%d", charsCount))
+	}
+
+	result := strings.Join(output, " ")
+
+	if sourceIsFile {
+		return result, filename
+	} else {
+		return result, ""
+	}
+
 }
 
 func main() {
 	result, filename := wc()
-	fmt.Printf("%s %s\n", result, filename)
+	if filename == "" {
+		fmt.Printf("%7s\n", result)
+	} else {
+		fmt.Printf("%7s %s\n", result, filename)
+	}
+
 }
